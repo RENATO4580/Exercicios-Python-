36a0929cd801c44cdb43ebd92700f9ca9a6d8e5d
Renato Lima Braga Santana <gury-santana@hotmail.com>
2026-06-08 11:52:46 -0300

    Estrutura: if..else..for

---
+++ for1.py
@@ -0,0 +1,7 @@
+# A estrutura de repetição for, é usada para 
+# ler lista, tuplas, dicionários ou sequencias
+# Neste exemplo usamos uma sequência com o comando
+# range(intervalo) que vai de 1 até 10 e 11 é 
+# o valor de saída 
+for i in range(1,11):
+    print(i)
